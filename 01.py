6f2fc40da18f88e447cd119aee4ee5f938db67c4
Scott Schwartz <Scott.Schwartz@xaxis.com>
2020-12-01 13:11:54 -0500

    inital commit and day 1

---
+++ 01.py
@@ -0,0 +1,36 @@
+from typing import List, Tuple
+
+from utils import read_file
+
+data = [int(n) for n in read_file('01')]
+
+
+# part 1
+def find_addends(nums: List[int], tgt: int) -> Tuple[int, int]:
+    num_map = {}
+    for i, num in enumerate(nums):
+        diff = tgt - num
+        if num_map.get(diff):
+            return nums[num_map[diff]], nums[i]
+        num_map[num] = i
+    return 'not found'
+
+
+x, y = find_addends(data, 2020)
+
+print('part 1', x * y)
+
+
+# part 2
+def find_three_addends(nums: List[int], tgt: int) -> Tuple[int, int]:
+    for i, num in enumerate(sorted(nums)):
+        new_tgt = tgt - num
+        addends = find_addends(nums, new_tgt)
+        if addends != 'not found':
+            a1, a2 = addends
+            return 2020 - new_tgt, a1, a2
+
+
+a1, a2, a3 = find_three_addends(data, 2020)
+
+print('part 2', a1 * a2 * a3)
